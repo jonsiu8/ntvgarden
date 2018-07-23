e6bce1b7f81bf73f483506fc2de8f2363509ca02
jon <jonsiu@gmail.com>
2018-07-23 12:12:36 +0800

    logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from "styled-components";
+import logo from "./logo.svg"
+
+const Logo = styled.img`
+  width: 105px;
+  margin-bottom: 0;
+
+  @media (max-width: 1023px) {
+    width: 70px;
+  }
+`;
 
 const Header = (props) => (
     <header id="header" className="alt">
-        <Link to="/" className="logo"><strong>NTV Garden</strong></Link>
+        {/*<Link to="/" className="logo"><strong>NTV Garden</strong></Link>*/}
+        <Link to="/" className="logo"><Logo src={logo}/></Link>
         <nav>
             <a className="menu-link" onClick={props.onToggleMenu} href="javascript:;">Menu</a>
         </nav>
